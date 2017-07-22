17a288707978a634d835fd3418be066ffed60371
Andy Haddon <lastcanti@gmail.com>
2017-07-22 19:55:54 -0400

    Create review1.py

---
+++ review1.py
@@ -0,0 +1,38 @@
+# Python 3 review
+
+# primitive datatypes and operators
+1 + 1
+1 / 1
+1 - 1
+1 * 1
+25.0 // 5.0
+
+# modulus operator returns remainder
+7 % 3
+
+# exponential
+2 ** 4
+
+# use parens to enforce priority
+(1+2) * 5
+
+# booleans
+True
+False
+
+# logical negation
+not True
+not False
+
+# logical and or
+True and True
+True or False
+
+2 != 2
+2 == 2
+
+# strings are created with '' or ""
+'Hello world'
+'Hello world'[0]
+len("Hello world")
+
